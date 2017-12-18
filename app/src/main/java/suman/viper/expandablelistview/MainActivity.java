02576b38c73be26447550f92ef0f80335171adf5
Suman Razz <viper.suman@gmail.com>
2017-12-18 15:54:40 +0545

    changed data

--- app/src/main/java/suman/viper/expandablelistview/MainActivity.java
+++ app/src/main/java/suman/viper/expandablelistview/MainActivity.java
@@ -103,37 +103,37 @@ public class MainActivity extends Activity {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("1st Semester");
+        listDataHeader.add("2nd Semester ");
+        listDataHeader.add("3rd Semester");
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> sem1 = new ArrayList<String>();
+        sem1.add("Information to IT");
+        sem1.add("Probability and Statistics");
+        sem1.add("Calculus");
+        sem1.add("Statistics I");
+        sem1.add("Fundamentals of C-Programming");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+        List<String> sem2 = new ArrayList<String>();
+        sem2.add("Statistics II");
+        sem2.add("Digital logic");
+        sem2.add("Microprocessor");
+        sem2.add("Linear Algebra");
+        sem2.add("Discrete structure");
+        sem2.add("Data Structure and Algorithm");
+
+        List<String> sem3 = new ArrayList<String>();
+        sem3.add("Object Oriented Programming");
+        sem3.add("Operating System");
+        sem3.add("Numerical Method");
+        sem3.add("Computer Architecture");
+        sem3.add("Fundamental of Management");
+
+        listDataChild.put(listDataHeader.get(0), sem1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), sem2);
+        listDataChild.put(listDataHeader.get(2), sem3);
     }
 }
